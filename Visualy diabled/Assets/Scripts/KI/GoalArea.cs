e86f0ac327c6fd209f2d12bc91d19142ff052d37
Greengrovers <s23102380@fhooe.at>
2026-05-15 11:38:34 +0200

    Block and goal area change

--- Visualy diabled/Assets/Scripts/KI/GoalArea.cs
+++ Visualy diabled/Assets/Scripts/KI/GoalArea.cs
@@ -1,37 +1,11 @@
 using UnityEngine;
-using System.Collections.Generic;
-
-public class GoalArea : MonoBehaviour
-{
-    public float triggerRadius = 2f;
-    public LayerMask sheepLayer;
-
-    private HashSet<SheepController> alreadyScored = new HashSet<SheepController>();
-
-    private void Update()
-    {
-        Collider[] hits = Physics.OverlapSphere(transform.position, triggerRadius, sheepLayer);
-
-        foreach (Collider hit in hits)
-        {
-            SheepController sheep = hit.GetComponentInParent<SheepController>();
-            if (sheep == null) continue;
-
-            if (alreadyScored.Contains(sheep)) continue;
-            alreadyScored.Add(sheep);
-
-            if (ScoreManager.Instance != null)
-            {
-                ScoreManager.Instance.AddSheepScore();
-            }
-
-            sheep.EnterGoal();
-        }
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.green;
-        Gizmos.DrawWireSphere(transform.position, triggerRadius);
-    }
-}
+ using System.Collections.Generic; 
+ public class GoalArea : MonoBehaviour
+ { public float triggerRadius = 0.01f; 
+ public LayerMask sheepLayer; private HashSet<SheepController> alreadyScored = new HashSet<SheepController>(); 
+ private void Update() { Collider[] hits = Physics.OverlapSphere(transform.position, triggerRadius, sheepLayer); 
+ foreach (Collider hit in hits) { SheepController sheep = hit.GetComponentInParent<SheepController>();
+  if (sheep == null) continue; if (alreadyScored.Contains(sheep)) continue; alreadyScored.Add(sheep);
+   if (ScoreManager.Instance != null) { ScoreManager.Instance.AddSheepScore(); } sheep.EnterGoal(); } }
+    private void OnDrawGizmosSelected() { Gizmos.color = Color.green; 
+    Gizmos.DrawWireSphere(transform.position, triggerRadius); }}
